d333f8e39ae49696ac4298d61a1054d6403b5087
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-10 09:59:14 -0700

    stuff

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log('connected');
+
+var arraySolutions = ['add', 'ade', 'and', 'dad', 'dam', 'den', 'end', 'mad', 'man', 'men', 'amen', 'dame', 'damn', 'dead', 'dean', 'made', 'mane', 'mead', 'mean', 'mend', 'name', 'amend', 'named', 'damned', 'demand', 'madden'];
+
+var scramble = 'nedmad';
+
+var guessedCorrect = []; 
